8f9b37ac1b5e259df64af3305b917c1b101e2764
Hokki Suwanda <hokkyss2@gmail.com>
2021-08-23 16:46:27 +0700

    add bio

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,26 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    width: {
+      content: "100vw",
+    },
+    height: {
+      content: "90vh",
+    },
+    fontSize: {
+      normal: "1.46vw",
+      big: "2.21vw",
+      heading: "2.93vw", // 40px in 1366px width
+    },
+    fontWeight: {
+      heading: 800,
+    },
+    letterSpacing: {
+      default: "0.06vw",
+    },
+    color: {
+      text: "black",
+    },
   },
   variants: {},
   plugins: [],
