d1d9d0554c0a7c77b9fcc8b16ca149fae52c59ed
Hokki Suwanda <hokkyss2@gmail.com>
2021-08-26 22:27:45 +0700

    add hover and go to page

--- components/elements/ProjectCard/ProjectCard.tsx
+++ components/elements/ProjectCard/ProjectCard.tsx
@@ -1,14 +1,31 @@
-import React from "react";
+/* eslint-disable @next/next/no-img-element */
+import React, { useState } from "react";
 
 import styles from "./ProjectCard.module.css";
 
 import Props from "@types";
+import { NormalText, TextIcon } from "@components/elements";
+
+import ArrowRight from "@icons/arrow-right.svg";
 
 export const ProjectCard = ({ item }: Props.ProjectCardProps): JSX.Element => {
   return (
-    <div className={`flex cursor-pointer ${styles.card}`}>
-      <div>{item.name}</div>
-      <div></div>
+    <div
+      className={`flex flex-col cursor-pointer ${styles.card} items-center justify-center`}
+    >
+      <div className={`${styles.imageContainer}`}>
+        <div
+          style={{
+            backgroundImage: `url(${item.preview})`,
+          }}
+          className={`absolute ${styles.preview}`}
+        />
+        <div className={`${styles.hoverContent}`}>
+          <a href={item.links.page}>
+            <TextIcon Icon={ArrowRight} text="See project" />
+          </a>
+        </div>
+      </div>
     </div>
   );
 };
