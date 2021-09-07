58f52259943c37e1d27b55ec1797edd2b323b7c3
hokkyss <hokkyss2@gmail.com>
2021-09-07 15:23:00 +0700

    change styling and make clicking good

--- components/elements/ProjectCard/ProjectCard.tsx
+++ components/elements/ProjectCard/ProjectCard.tsx
@@ -4,21 +4,21 @@ import React, { useState } from "react";
 import styles from "./ProjectCard.module.css";
 
 import Props from "@types";
-import { NormalText, TextIcon } from "@components/elements";
+import { TextIcon } from "@components/elements";
 
 import ArrowRight from "@icons/arrow-right.svg";
 
 export const ProjectCard = ({ item }: Props.ProjectCardProps): JSX.Element => {
   return (
     <div
-      className={`flex flex-col cursor-pointer ${styles.card} items-center justify-center`}
+      className={`flex flex-col cursor-pointer ${styles.card} ${styles.hovering} items-center justify-center`}
     >
       <div className={`${styles.imageContainer}`}>
         <div
           style={{
             backgroundImage: `url(${item.preview})`,
           }}
-          className={`absolute ${styles.preview} ${styles.hover}`}
+          className={`absolute ${styles.preview} ${styles.hovering}`}
         />
         <div className={`${styles.hoverContent}`}>
           <a href={item.links.page}>
