7fc161ad26b0ccf22d7312461ac16e5f1e49a2ac
Hokki Suwanda <hokkyss2@gmail.com>
2022-02-23 20:19:22 +0700

    setup react

--- src/index.tsx
+++ src/index.tsx
@@ -6,10 +6,10 @@ import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("hokkyss")
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById("hokkyss")
 );
 
 // If you want your app to work offline and load faster, you can change
