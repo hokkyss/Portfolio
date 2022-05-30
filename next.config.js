ce0851c0a6c22b09e25009bc3aba94145041bf0f
Hokki Suwanda <hokkyss2@gmail.com>
2022-05-30 17:42:02 +0700

    disable pwa

--- next.config.js
+++ next.config.js
@@ -15,7 +15,7 @@ const nextConfig = {
 	},
 	pwa: {
 		dest: 'public',
-		disable: process.env.NODE_ENV === 'development',
+		disable: true /* process.env.NODE_ENV === 'development' */,
 	},
 	eslint: {
 		ignoreDuringBuilds: true,
