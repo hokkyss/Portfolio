94e17f4e59a8f2de6dcb6f6c56a727187da70e8e
Hokki Suwanda <hokkyss2@gmail.com>
2022-06-05 21:41:11 +0700

    change hokkyss to Hokki Suwanda

--- utils/firebase.ts
+++ utils/firebase.ts
@@ -1,5 +1,14 @@
 import { initializeApp } from 'firebase/app'
-import { DocumentData, FirestoreDataConverter } from 'firebase/firestore'
+import { getAnalytics, isSupported } from 'firebase/analytics'
+import {
+	DocumentData,
+	FirestoreDataConverter,
+	getFirestore,
+} from 'firebase/firestore'
+import { getDatabase } from 'firebase/database'
+import { getAuth } from 'firebase/auth'
+import { getStorage } from 'firebase/storage'
+import { getPerformance } from 'firebase/performance'
 
 import firebaseConfig from 'firebase-config.json'
 
@@ -11,3 +20,12 @@ export const makeConverter = <
 	toFirestore: (data: T): DocumentData => ({ ...data }),
 	fromFirestore: (snapshot): T => snapshot.data() as T,
 })
+
+const auth = getAuth(firebaseApp)
+const firestore = getFirestore(firebaseApp)
+const database = getDatabase(firebaseApp)
+const analytics =
+	typeof window === 'undefined' ? null : getAnalytics(firebaseApp)
+const storage = getStorage(firebaseApp)
+const performance =
+	typeof window === 'undefined' ? null : getPerformance(firebaseApp)
