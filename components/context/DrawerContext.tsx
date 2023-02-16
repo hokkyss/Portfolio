268ece8c44bc8ea3ae26c33b696be249322ab676
Hokki Suwanda <hokkyss2@gmail.com>
2023-02-16 12:59:01 +0000

    fix: component type

--- components/context/DrawerContext.tsx
+++ components/context/DrawerContext.tsx
@@ -4,20 +4,22 @@ import { useBreakpointValue } from '~/hooks'
 
 export const DrawerContext = React.createContext<boolean>(false)
 
-export const DrawerProvider = React.memo(function DrawerProvider({ children }) {
-	const breakpoint = useBreakpointValue({
-		base: true,
-		xs: false,
-		sm: false,
-		md: false,
-		lg: false,
-		xl: false,
-		'2xl': false,
-	})
+export const DrawerProvider = React.memo<React.PropsWithChildren>(
+	function DrawerProvider({ children }) {
+		const breakpoint = useBreakpointValue({
+			base: true,
+			xs: false,
+			sm: false,
+			md: false,
+			lg: false,
+			xl: false,
+			'2xl': false,
+		})
 
-	return (
-		<DrawerContext.Provider value={breakpoint}>
-			{children}
-		</DrawerContext.Provider>
-	)
-})
+		return (
+			<DrawerContext.Provider value={breakpoint}>
+				{children}
+			</DrawerContext.Provider>
+		)
+	}
+)
