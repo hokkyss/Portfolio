a8ed6c9dc192749f0061d59422073ce5a45a517d
Hokki Suwanda <hokkyss2@gmail.com>
2023-02-19 04:08:04 +0000

    fix: underdevelopment date

--- lib/common/constants/underDevelopment.ts
+++ lib/common/constants/underDevelopment.ts
@@ -1,6 +1,6 @@
 export const underDevelopment: Blog = {
-	createdAt: new Date('2022-04-20T09:09:27.107Z'),
-	updatedAt: new Date('2022-07-15T00:00:00.000Z'),
+	createdAt: '2022-04-20T09:09:27.107Z',
+	updatedAt: '2022-07-15T00:00:00.000Z',
 	slug: 'under-development',
 	title: 'Under Development',
 	subtitle: 'Sorry! This section is under development.',
