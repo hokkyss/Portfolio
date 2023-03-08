e181d2e6b795e465971641fb95452263be2a13c4
Hokki Suwanda <hokkyss2@gmail.com>
2023-03-08 17:13:38 +0000

    feat: add /admin/blogs

--- lib/common/date.ts
+++ lib/common/date.ts
@@ -23,6 +23,17 @@ const months = [
 	'Dec',
 ]
 
+export const formatDateTime = (isostringOrDate: string | Date) => {
+	const date =
+		typeof isostringOrDate === 'string'
+			? new Date(isostringOrDate)
+			: isostringOrDate
+
+	return `${days[date.getUTCDay()]}, ${
+		months[date.getUTCMonth()]
+	} ${date.getUTCDate()}, ${date.getUTCFullYear()}: ${date.getUTCHours()}:${date.getUTCMinutes()}:${date.getUTCSeconds()} UTC`
+}
+
 export const formatDate = (isostringOrDate: string | Date): string => {
 	const date =
 		typeof isostringOrDate === 'string'
