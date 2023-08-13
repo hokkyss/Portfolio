84f8716b57549fd78a5f6718f530becb5c5d5c6e
Hokki Suwanda <hokkyss2@gmail.com>
2023-08-13 10:59:22 +0700

    fix: lg screen

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-import 'server-only';
-
 import NextLogo from './_libs/_common/_assets/next.svg';
 import TodoService from './_libs/_common/_services/todo.service';
 
@@ -7,14 +5,14 @@ export default async function Home() {
   const todos = await TodoService.getTodos();
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between">
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
         <NextLogo className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert" height={37} width={180} />
       </div>
       {todos.map((todo) => (
         <p key={todo.id}>{todo.title}</p>
       ))}
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+      <div className="grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
