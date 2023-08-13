3143cb1eef42bc360e05a049d2fdff6cf145b5ac
Hokki Suwanda <hokkyss2@gmail.com>
2023-08-13 18:21:56 +0700

    fix: layout for smaller screen

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,19 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata: Metadata = {
   colorScheme: 'light dark',
   description:
-    "I'm Hokki Suwanda, a fullstack software engineer. A Computer Science Fresh Graduate. A graduated undergraduate student full of enthusiasm and motivation with problem solving capabilities. Very excited to learn something new. Always fulfilling all responsibilities heartfully. Pursuing career as a fullstack software engineer. Used quite lot of tech stacks.",
+    "I'm Hokki Suwanda, a fullstack software engineer. A Computer Science Fresh Graduate. Full of enthusiasm and motivation with problem solving capabilities. Very excited to learn something new. Always fulfilling all responsibilities wholeheartedly. Pursuing career as a fullstack software engineer. Used quite lot of tech stacks.",
+  keywords: [
+    'Hokki Suwanda',
+    'hokkyss',
+    'software engineer',
+    'fullstack engineer',
+    'front end engineer',
+    'back end engineer',
+    'web developer',
+    'ITB',
+    'Bandung Institute of Technology',
+    'Institut Teknologi Bandung',
+  ],
   title: 'hokkyss | Hokki Suwanda',
 };
 
