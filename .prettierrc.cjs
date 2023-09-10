f5369661e48a61663884cbb1f1ee57929f0a07d8
Hokki Suwanda <hokki.suwanda@nri.co.id>
2023-09-10 09:34:25 +0700

    feat: add eslint config

---
+++ .prettierrc.cjs
@@ -0,0 +1,13 @@
+const alloyConfig = require('eslint-config-alloy/.prettierrc');
+
+/** @type {import("prettier").Config} */
+module.exports = {
+	...alloyConfig,
+	useTabs: true,
+	singleQuote: true,
+	trailingComma: 'none',
+	printWidth: 100,
+	plugins: ['prettier-plugin-svelte'],
+	pluginSearchDirs: ['.'],
+	overrides: [{ files: '*.svelte', options: { parser: 'svelte' } }]
+};
