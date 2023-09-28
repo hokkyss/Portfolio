b053bf75d668fa12ddca755490041c8b1f84e3da
Hokki Suwanda <hokkyss2@gmail.com>
2023-09-29 00:17:29 +0700

    refactor: file structure

---
+++ app/_server/_molecules/tech-stack.molecule.tsx
@@ -0,0 +1,32 @@
+import 'server-only';
+
+import type { TechStack as TechStackModel } from '~/_common/_models/tech-stack.model';
+
+import { memo } from 'react';
+
+import TechStackChip from '../_atoms/tech-stack-chip.atom';
+
+interface TechStackProps {
+  techStacks: TechStackModel[];
+  title: string;
+}
+
+const containerStyle =
+  'flex flex-row justify-center flex-wrap gap-2 relative w-full border border-black dark:border-white hover:border-blue-700 hover:dark:border-green-300 rounded-lg px-3 pb-2 pt-5 transition-all duration-300 hover:text-blue-700 hover:dark:text-green-300 selection:dark:text-black selection:dark:bg-green-300';
+const titleStyle = 'px-2 absolute -top-[14px] backdrop-blur-xl text-xl';
+const chipStyle = 'hover:text-blue-700 hover:dark:fill-green-300 hover:dark:text-green-300';
+
+const TechStack = memo<TechStackProps>(async (props) => {
+  const { techStacks, title } = props;
+
+  return (
+    <div className={containerStyle}>
+      <h1 className={titleStyle}>{title}</h1>
+      {techStacks.map((techStack) => (
+        <TechStackChip className={chipStyle} key={techStack.name} techStack={techStack} />
+      ))}
+    </div>
+  );
+});
+
+export default TechStack;
