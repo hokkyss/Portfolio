0bc1f80a96a4890e902e98217edcd3b6cb200f39
Hokki Suwanda <hokki.suwanda@nri.co.id>
2023-11-03 17:28:11 +0700

    chore: code splitting and removing unused files

--- app/_client/atoms/button.atom.tsx
+++ app/_client/atoms/button.atom.tsx
@@ -1,6 +1,9 @@
 'use client';
 
+import 'client-only';
+
 import type { ButtonVariants } from '~/_common/styles/button.style';
+import type { SetRequired } from '~/_common/types/common.type';
 
 import { Slot } from '@radix-ui/react-slot';
 import * as React from 'react';
@@ -8,7 +11,9 @@ import * as React from 'react';
 import buttonVariants from '~/_common/styles/button.style';
 import { cn } from '~/_common/utils/classname.util';
 
-export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement>, ButtonVariants {
+export interface ButtonProps
+  extends SetRequired<React.ButtonHTMLAttributes<HTMLButtonElement>, 'type'>,
+    ButtonVariants {
   asChild?: boolean;
 }
 
