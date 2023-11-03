d0d786c2d1b9f1188b75c54543544c1deefe76ac
Hokki Suwanda <hokkyss2@gmail.com>
2023-11-03 15:47:28 +0700

    fix: cursor highlight color

--- app/_client/atoms/cursor.atom.tsx
+++ app/_client/atoms/cursor.atom.tsx
@@ -2,30 +2,13 @@
 
 import 'client-only';
 
-import { autoUpdate, offset, shift, useFloating } from '@floating-ui/react';
-import { memo, useEffect, useMemo, useState } from 'react';
+import { autoUpdate, offset, shift, useClientPoint, useFloating, useInteractions, useRole } from '@floating-ui/react';
+import { memo } from 'react';
 
 import { tw } from '~/_common/utils/classname.util';
 
 const Cursor = memo(() => {
-  const [cursorPosition, setCursorPosition] = useState<{ x: number; y: number }>({ x: 0, y: 0 });
-  const referenceElement = useMemo(
-    () => ({
-      getBoundingClientRect: () => ({
-        bottom: cursorPosition.y,
-        height: 0,
-        left: cursorPosition.x,
-        right: cursorPosition.x,
-        top: cursorPosition.y,
-        width: 0,
-        x: cursorPosition.x,
-        y: cursorPosition.y,
-      }),
-    }),
-    [cursorPosition.x, cursorPosition.y],
-  );
-
-  const { floatingStyles, refs } = useFloating({
+  const { context, floatingStyles, refs } = useFloating({
     middleware: [
       offset((state) => ({
         crossAxis: 0,
@@ -39,27 +22,16 @@ const Cursor = memo(() => {
     whileElementsMounted: autoUpdate,
   });
 
-  useEffect(() => {
-    refs.setReference(referenceElement);
-  }, [referenceElement, refs]);
-
-  useEffect(() => {
-    const handler = ({ clientX, clientY }: MouseEvent) => {
-      setCursorPosition({
-        x: clientX,
-        y: clientY,
-      });
-    };
-    document.body.addEventListener('mousemove', handler);
-
-    return () => document.body.removeEventListener('mousemove', handler);
-  }, []);
+  const clientPoint = useClientPoint(context, { axis: 'both' });
+  const role = useRole(context);
+  const { getFloatingProps } = useInteractions([clientPoint, role]);
 
   return (
     <span
-      className={tw`bg-green-600 -z-[10] rounded-full blur-lg w-20 h-20`}
+      className={tw`bg-slate-600 -z-[10] rounded-full blur-2xl w-40 h-40`}
       ref={refs.setFloating}
       style={floatingStyles}
+      {...getFloatingProps()}
     />
   );
 });
