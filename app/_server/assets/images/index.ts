7610fb5061fb1f04d54f81bf64ddd3b84c4e7b69
Hokki Suwanda <hokkyss2@gmail.com>
2023-11-05 19:22:09 +0700

    refactor: add project card

--- app/_server/assets/images/index.ts
+++ app/_server/assets/images/index.ts
@@ -1,7 +1,5 @@
 import 'server-only';
 
 export { default as me } from './me.png';
-export { default as reshiram } from './reshiram.png';
 export { default as thisWebsite } from './this-website.png';
 export { default as urlShortener } from './url-shortener.png';
-export { default as zekrom } from './zekrom.png';
