90a582d561e5bf25bc0fc6c2c117698e4294f1c0
Hokki Suwanda <hokki.suwanda@nri.co.id>
2023-11-08 10:52:38 +0700

    fix: change development environment

--- app/_common/configs/env.config.ts
+++ app/_common/configs/env.config.ts
@@ -1,7 +1,9 @@
 const envConfig = Object.freeze({
   __DEV__:
     process.env.NEXT_PUBLIC_VERCEL_ENV === 'development' ||
+    process.env.NEXT_PUBLIC_VERCEL_ENV === 'preview' ||
     process.env.VERCEL_ENV === 'development' ||
+    process.env.VERCEL_ENV === 'preview' ||
     process.env.NODE_ENV === 'development',
   appUrl:
     process.env.VERCEL_URL || process.env.NEXT_PUBLIC_VERCEL_URL
