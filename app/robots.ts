2a976ccedd177c6b2189ebffdd5bd1f762aa4347
Hokki Suwanda <hokki.suwanda@nri.co.id>
2023-11-08 11:05:38 +0700

    fix: robot

--- app/robots.ts
+++ app/robots.ts
@@ -5,8 +5,8 @@ import envConfig from './_common/configs/env.config';
 export default function generateRobots(): MetadataRoute.Robots {
   return {
     rules: {
-      allow: '/',
-      disallow: '/private/',
+      allow: envConfig.__DEV__ ? undefined : '/',
+      disallow: envConfig.__DEV__ ? '*' : '/private',
       userAgent: '*',
     },
     sitemap: new URL('/sitemap.xml', envConfig.appUrl).toString(),
