fb79716cc7ef653d1c06e217a3f2da1acc5b2ee9
Hokki Suwanda <hokkyss2@gmail.com>
2023-11-12 11:50:03 +0700

    fix: use static metadata instead:

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,8 @@
 import 'server-only';
 import './globals.css';
 
-import type { Metadata, ResolvingMetadata, Viewport } from 'next';
+import type { Metadata, Viewport } from 'next';
 import type { PropsWithChildren } from 'react';
-
-import type { EmptyObject } from './_common/types/common.type';
 
 import { Analytics } from '@vercel/analytics/react';
 import { Inter } from 'next/font/google';
@@ -16,53 +14,49 @@ import { cn, tw } from './_common/utils/classname.util';
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-inter' });
 
-export function generateViewport(): Viewport {
-  return {
-    colorScheme: 'light dark',
-    height: 'device-height',
-    initialScale: 1.0,
-    maximumScale: 1.0,
-    minimumScale: 1.0,
-    width: 'device-width',
-  };
-}
+export const viewport: Viewport = {
+  colorScheme: 'light dark',
+  height: 'device-height',
+  initialScale: 1.0,
+  maximumScale: 1.0,
+  minimumScale: 1.0,
+  width: 'device-width',
+};
 
-export async function generateMetadata(_props: EmptyObject, _parent: ResolvingMetadata): Promise<Metadata> {
-  return {
-    applicationName: 'Hokki Suwanda Portfolio',
-    authors: [{ name: 'Hokki Suwanda', url: 'https://github.com/hokkyss' }],
-    creator: 'Hokki Suwanda',
-    description:
-      "I'm Hokki Suwanda, a fullstack software engineer. A Computer Science Fresh Graduate. Full of enthusiasm and motivation with problem solving capabilities. Very excited to learn something new. Always fulfilling all responsibilities wholeheartedly. Pursuing career as a fullstack software engineer. Used quite lot of tech stacks.",
-    generator: 'Next.js',
-    keywords: [
-      'Hokki Suwanda',
-      'hokkyss',
-      'software engineer',
-      'fullstack engineer',
-      'front end engineer',
-      'back end engineer',
-      'web developer',
-      'ITB',
-      'Bandung Institute of Technology',
-      'Institut Teknologi Bandung',
-      'competitive programming',
-    ],
-    publisher: 'Vercel',
-    robots: {
-      googleBot: {
-        index: !envConfig.__DEV__,
-        notranslate: true,
-      },
+export const metadata: Metadata = {
+  applicationName: 'Hokki Suwanda Portfolio',
+  authors: [{ name: 'Hokki Suwanda', url: 'https://github.com/hokkyss' }],
+  creator: 'Hokki Suwanda',
+  description:
+    "I'm Hokki Suwanda, a fullstack software engineer. A Computer Science Fresh Graduate. Full of enthusiasm and motivation with problem solving capabilities. Very excited to learn something new. Always fulfilling all responsibilities wholeheartedly. Pursuing career as a fullstack software engineer. Used quite lot of tech stacks.",
+  generator: 'Next.js',
+  keywords: [
+    'Hokki Suwanda',
+    'hokkyss',
+    'software engineer',
+    'fullstack engineer',
+    'front end engineer',
+    'back end engineer',
+    'web developer',
+    'ITB',
+    'Bandung Institute of Technology',
+    'Institut Teknologi Bandung',
+    'competitive programming',
+  ],
+  publisher: 'Vercel',
+  robots: {
+    googleBot: {
       index: !envConfig.__DEV__,
       notranslate: true,
     },
-    title: {
-      default: 'hokkyss | Hokki Suwanda',
-      template: 'hokkyss | %s',
-    },
-  };
-}
+    index: !envConfig.__DEV__,
+    notranslate: true,
+  },
+  title: {
+    default: 'hokkyss | Hokki Suwanda',
+    template: 'hokkyss | %s',
+  },
+};
 
 export default async function Layout({ children }: PropsWithChildren) {
   return (
