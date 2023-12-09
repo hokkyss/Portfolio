75287157dc85bade1f1d84186ef3e9ab315e2f88
Hokki Suwanda <hokkyss2@gmail.com>
2023-12-09 12:25:58 +0700

    fix: update sitemap

--- app/_server/configs/env.config.ts
+++ app/_server/configs/env.config.ts
@@ -1,10 +1,10 @@
 import 'server-only';
 
-const serverEnvConfig = Object.freeze({
+const serverEnvConfig = {
   cmsApiVersion: process.env.CMS_API_VERSION || '2023-11-07',
   cmsDataset: process.env.CMS_DATASET || '',
   cmsProjectId: process.env.CMS_PROJECT_ID || '',
   cmsToken: process.env.CMS_TOKEN || '',
-});
+} as const;
 
 export default serverEnvConfig;
