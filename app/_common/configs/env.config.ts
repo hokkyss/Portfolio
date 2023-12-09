3c54145e7eef9c9cfff50290f2ee19c6ea764913
Hokki Suwanda <hokki.suwanda@nri.co.id>
2023-12-09 12:29:39 +0700

    feat: add domain environment variable

--- app/_common/configs/env.config.ts
+++ app/_common/configs/env.config.ts
@@ -10,6 +10,10 @@ const envConfig = {
       return `https://i-am.hokkyss.com`;
     }
 
+    if (process.env.NEXT_PUBLIC_DOMAIN) {
+      return `https://${process.env.NEXT_PUBLIC_DOMAIN}/`;
+    }
+
     if (process.env.VERCEL_URL || process.env.NEXT_PUBLIC_VERCEL_URL) {
       return `https://${process.env.VERCEL_URL || process.env.NEXT_PUBLIC_VERCEL_URL}/`;
     }
