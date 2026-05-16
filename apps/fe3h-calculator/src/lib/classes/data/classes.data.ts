be8d4492a13c777065dcfa076ed3edf99fbd5936
Hokki Suwanda <hokkyss2@gmail.com>
2026-05-16 19:59:12 +0700

    chore(fe3h-calculator): fix unique class growth rates

--- apps/fe3h-calculator/src/lib/classes/data/classes.data.ts
+++ apps/fe3h-calculator/src/lib/classes/data/classes.data.ts
@@ -11,7 +11,6 @@ const COMMONER: ClassData = {
   id: 'commoner',
   movement: 4,
   name: 'Commoner',
-  statCaps: { cha: 26, def: 20, dex: 20, hp: 40, lck: 25, mag: 20, res: 20, spd: 20, str: 20 },
   tier: 'Beginner',
 };
 
@@ -21,7 +20,6 @@ const NOBLE: ClassData = {
   id: 'noble',
   movement: 4,
   name: 'Noble',
-  statCaps: { cha: 26, def: 20, dex: 20, hp: 40, lck: 25, mag: 20, res: 20, spd: 20, str: 20 },
   tier: 'Beginner',
 };
 
@@ -31,7 +29,6 @@ const MYRMIDON: ClassData = {
   id: 'myrmidon',
   movement: 4,
   name: 'Myrmidon',
-  statCaps: { cha: 26, def: 20, dex: 25, hp: 40, lck: 25, mag: 20, res: 20, spd: 25, str: 20 },
   tier: 'Beginner',
 };
 
@@ -41,7 +38,6 @@ const SOLDIER: ClassData = {
   id: 'soldier',
   movement: 4,
   name: 'Soldier',
-  statCaps: { cha: 26, def: 23, dex: 20, hp: 40, lck: 25, mag: 20, res: 20, spd: 20, str: 23 },
   tier: 'Beginner',
 };
 
@@ -51,7 +47,6 @@ const FIGHTER: ClassData = {
   id: 'fighter',
   movement: 4,
   name: 'Fighter',
-  statCaps: { cha: 26, def: 20, dex: 20, hp: 45, lck: 25, mag: 20, res: 20, spd: 20, str: 25 },
   tier: 'Beginner',
 };
 
@@ -61,7 +56,6 @@ const MONK: ClassData = {
   id: 'monk',
   movement: 4,
   name: 'Monk',
-  statCaps: { cha: 26, def: 20, dex: 20, hp: 40, lck: 25, mag: 25, res: 23, spd: 20, str: 20 },
   tier: 'Beginner',
 };
 
@@ -73,7 +67,6 @@ const LORD: ClassData = {
   id: 'lord',
   movement: 5,
   name: 'Lord',
-  statCaps: { cha: 30, def: 23, dex: 25, hp: 45, lck: 29, mag: 22, res: 22, spd: 22, str: 25 },
   tier: 'Intermediate',
 };
 
@@ -83,7 +76,6 @@ const MERCENARY: ClassData = {
   id: 'mercenary',
   movement: 5,
   name: 'Mercenary',
-  statCaps: { cha: 28, def: 22, dex: 27, hp: 45, lck: 28, mag: 20, res: 20, spd: 27, str: 25 },
   tier: 'Intermediate',
 };
 
@@ -93,7 +85,6 @@ const THIEF: ClassData = {
   id: 'thief',
   movement: 5,
   name: 'Thief',
-  statCaps: { cha: 28, def: 20, dex: 25, hp: 43, lck: 30, mag: 20, res: 20, spd: 30, str: 23 },
   tier: 'Intermediate',
 };
 
@@ -103,7 +94,6 @@ const CAVALIER: ClassData = {
   id: 'cavalier',
   movement: 7,
   name: 'Cavalier',
-  statCaps: { cha: 27, def: 26, dex: 22, hp: 46, lck: 27, mag: 20, res: 20, spd: 22, str: 26 },
   tier: 'Intermediate',
 };
 
@@ -113,7 +103,6 @@ const ARMOR_KNIGHT: ClassData = {
   id: 'armor-knight',
   movement: 4,
   name: 'Armor Knight',
-  statCaps: { cha: 24, def: 34, dex: 20, hp: 50, lck: 25, mag: 18, res: 22, spd: 18, str: 30 },
   tier: 'Intermediate',
 };
 
@@ -123,7 +112,6 @@ const BRIGAND: ClassData = {
   id: 'brigand',
   movement: 5,
   name: 'Brigand',
-  statCaps: { cha: 25, def: 22, dex: 22, hp: 50, lck: 25, mag: 18, res: 18, spd: 22, str: 30 },
   tier: 'Intermediate',
 };
 
@@ -133,7 +121,6 @@ const ARCHER: ClassData = {
   id: 'archer',
   movement: 4,
   name: 'Archer',
-  statCaps: { cha: 27, def: 22, dex: 28, hp: 43, lck: 27, mag: 20, res: 20, spd: 25, str: 24 },
   tier: 'Intermediate',
 };
 
@@ -143,7 +130,6 @@ const BRAWLER: ClassData = {
   id: 'brawler',
   movement: 5,
   name: 'Brawler',
-  statCaps: { cha: 25, def: 22, dex: 25, hp: 47, lck: 25, mag: 18, res: 18, spd: 28, str: 30 },
   tier: 'Intermediate',
 };
 
@@ -153,7 +139,6 @@ const MAGE: ClassData = {
   id: 'mage',
   movement: 4,
   name: 'Mage',
-  statCaps: { cha: 27, def: 19, dex: 22, hp: 40, lck: 27, mag: 30, res: 25, spd: 22, str: 18 },
   tier: 'Intermediate',
 };
 
@@ -163,7 +148,6 @@ const DARK_MAGE: ClassData = {
   id: 'dark-mage',
   movement: 4,
   name: 'Dark Mage',
-  statCaps: { cha: 24, def: 20, dex: 22, hp: 45, lck: 24, mag: 30, res: 22, spd: 22, str: 20 },
   tier: 'Intermediate',
 };
 
@@ -173,7 +157,6 @@ const PRIEST: ClassData = {
   id: 'priest',
   movement: 4,
   name: 'Priest',
-  statCaps: { cha: 28, def: 18, dex: 22, hp: 40, lck: 27, mag: 27, res: 28, spd: 22, str: 18 },
   tier: 'Intermediate',
 };
 
@@ -185,7 +168,6 @@ const HERO: ClassData = {
   id: 'hero',
   movement: 5,
   name: 'Hero',
-  statCaps: { cha: 30, def: 25, dex: 30, hp: 55, lck: 30, mag: 20, res: 20, spd: 30, str: 33 },
   tier: 'Advanced',
 };
 
@@ -195,7 +177,6 @@ const SWORDMASTER: ClassData = {
   id: 'swordmaster',
   movement: 6,
   name: 'Swordmaster',
-  statCaps: { cha: 30, def: 23, dex: 33, hp: 50, lck: 30, mag: 20, res: 20, spd: 37, str: 28 },
   tier: 'Advanced',
 };
 
@@ -205,7 +186,6 @@ const ASSASSIN: ClassData = {
   id: 'assassin',
   movement: 6,
   name: 'Assassin',
-  statCaps: { cha: 30, def: 22, dex: 32, hp: 50, lck: 35, mag: 20, res: 22, spd: 35, str: 28 },
   tier: 'Advanced',
 };
 
@@ -215,7 +195,6 @@ const FORTRESS_KNIGHT: ClassData = {
   id: 'fortress-knight',
   movement: 4,
   name: 'Fortress Knight',
-  statCaps: { cha: 25, def: 42, dex: 22, hp: 60, lck: 25, mag: 18, res: 25, spd: 18, str: 38 },
   tier: 'Advanced',
 };
 
@@ -225,7 +204,6 @@ const PALADIN: ClassData = {
   id: 'paladin',
   movement: 8,
   name: 'Paladin',
-  statCaps: { cha: 30, def: 30, dex: 28, hp: 55, lck: 30, mag: 22, res: 22, spd: 25, str: 33 },
   tier: 'Advanced',
 };
 
@@ -235,7 +213,6 @@ const WYVERN_RIDER: ClassData = {
   id: 'wyvern-rider',
   movement: 7,
   name: 'Wyvern Rider',
-  statCaps: { cha: 28, def: 30, dex: 25, hp: 55, lck: 25, mag: 18, res: 18, spd: 25, str: 35 },
   tier: 'Advanced',
 };
 
@@ -245,7 +222,6 @@ const WARRIOR: ClassData = {
   id: 'warrior',
   movement: 5,
   name: 'Warrior',
-  statCaps: { cha: 27, def: 25, dex: 25, hp: 65, lck: 25, mag: 18, res: 18, spd: 22, str: 40 },
   tier: 'Advanced',
 };
 
@@ -255,7 +231,6 @@ const SNIPER: ClassData = {
   id: 'sniper',
   movement: 5,
   name: 'Sniper',
-  statCaps: { cha: 30, def: 25, dex: 38, hp: 55, lck: 30, mag: 20, res: 22, spd: 30, str: 30 },
   tier: 'Advanced',
 };
 
@@ -265,7 +240,6 @@ const GRAPPLER: ClassData = {
   id: 'grappler',
   movement: 5,
   name: 'Grappler',
-  statCaps: { cha: 26, def: 25, dex: 30, hp: 60, lck: 25, mag: 18, res: 18, spd: 32, str: 40 },
   tier: 'Advanced',
 };
 
@@ -275,7 +249,6 @@ const WARLOCK: ClassData = {
   id: 'warlock',
   movement: 4,
   name: 'Warlock',
-  statCaps: { cha: 27, def: 20, dex: 27, hp: 45, lck: 27, mag: 40, res: 27, spd: 22, str: 18 },
   tier: 'Advanced',
 };
 
@@ -285,7 +258,6 @@ const BISHOP: ClassData = {
   id: 'bishop',
   movement: 4,
   name: 'Bishop',
-  statCaps: { cha: 30, def: 19, dex: 25, hp: 45, lck: 30, mag: 35, res: 35, spd: 22, str: 18 },
   tier: 'Advanced',
 };
 
@@ -295,7 +267,6 @@ const DARK_BISHOP: ClassData = {
   id: 'dark-bishop',
   movement: 4,
   name: 'Dark Bishop',
-  statCaps: { cha: 25, def: 22, dex: 24, hp: 48, lck: 24, mag: 38, res: 30, spd: 20, str: 18 },
   tier: 'Advanced',
 };
 
@@ -305,7 +276,6 @@ const VALKYRIE: ClassData = {
   id: 'valkyrie',
   movement: 8,
   name: 'Valkyrie',
-  statCaps: { cha: 30, def: 22, dex: 27, hp: 48, lck: 28, mag: 35, res: 30, spd: 30, str: 20 },
   tier: 'Advanced',
 };
 
@@ -315,7 +285,6 @@ const PEGASUS_KNIGHT: ClassData = {
   id: 'pegasus-knight',
   movement: 7,
   name: 'Pegasus Knight',
-  statCaps: { cha: 30, def: 22, dex: 28, hp: 45, lck: 30, mag: 25, res: 27, spd: 32, str: 25 },
   tier: 'Advanced',
 };
 
@@ -327,7 +296,6 @@ const FALCON_KNIGHT: ClassData = {
   id: 'falcon-knight',
   movement: 8,
   name: 'Falcon Knight',
-  statCaps: { cha: 35, def: 24, dex: 33, hp: 55, lck: 35, mag: 28, res: 32, spd: 42, str: 30 },
   tier: 'Master',
 };
 
@@ -337,7 +305,6 @@ const WYVERN_LORD: ClassData = {
   id: 'wyvern-lord',
   movement: 8,
   name: 'Wyvern Lord',
-  statCaps: { cha: 30, def: 38, dex: 28, hp: 65, lck: 28, mag: 18, res: 18, spd: 28, str: 43 },
   tier: 'Master',
 };
 
@@ -347,7 +314,6 @@ const MORTAL_SAVANT: ClassData = {
   id: 'mortal-savant',
   movement: 6,
   name: 'Mortal Savant',
-  statCaps: { cha: 32, def: 24, dex: 35, hp: 55, lck: 30, mag: 35, res: 25, spd: 37, str: 28 },
   tier: 'Master',
 };
 
@@ -357,7 +323,6 @@ const GREAT_KNIGHT: ClassData = {
   id: 'great-knight',
   movement: 6,
   name: 'Great Knight',
-  statCaps: { cha: 28, def: 45, dex: 25, hp: 65, lck: 28, mag: 20, res: 25, spd: 22, str: 42 },
   tier: 'Master',
 };
 
@@ -367,7 +332,6 @@ const BOW_KNIGHT: ClassData = {
   id: 'bow-knight',
   movement: 8,
   name: 'Bow Knight',
-  statCaps: { cha: 35, def: 27, dex: 40, hp: 60, lck: 33, mag: 20, res: 23, spd: 37, str: 35 },
   tier: 'Master',
 };
 
@@ -377,7 +341,6 @@ const DARK_KNIGHT: ClassData = {
   id: 'dark-knight',
   movement: 8,
   name: 'Dark Knight',
-  statCaps: { cha: 30, def: 30, dex: 28, hp: 58, lck: 28, mag: 38, res: 25, spd: 28, str: 33 },
   tier: 'Master',
 };
 
@@ -387,7 +350,6 @@ const HOLY_KNIGHT: ClassData = {
   id: 'holy-knight',
   movement: 8,
   name: 'Holy Knight',
-  statCaps: { cha: 33, def: 28, dex: 28, hp: 58, lck: 33, mag: 37, res: 35, spd: 28, str: 30 },
   tier: 'Master',
 };
 
@@ -397,7 +359,6 @@ const WAR_MASTER: ClassData = {
   id: 'war-master',
   movement: 5,
   name: 'War Master',
-  statCaps: { cha: 28, def: 28, dex: 35, hp: 70, lck: 28, mag: 18, res: 18, spd: 35, str: 50 },
   tier: 'Master',
 };
 
@@ -407,7 +368,6 @@ const GREMORY: ClassData = {
   id: 'gremory',
   movement: 5,
   name: 'Gremory',
-  statCaps: { cha: 35, def: 20, dex: 28, hp: 50, lck: 35, mag: 45, res: 38, spd: 25, str: 18 },
   tier: 'Master',
 };
 
@@ -419,7 +379,6 @@ const DANCER: ClassData = {
   id: 'dancer',
   movement: 5,
   name: 'Dancer',
-  statCaps: { cha: 40, def: 22, dex: 28, hp: 50, lck: 35, mag: 24, res: 24, spd: 35, str: 24 },
   tier: 'Special',
 };
 
@@ -431,7 +390,6 @@ const ARMORED_LORD: ClassData = {
   id: 'armored-lord',
   movement: 4,
   name: 'Armored Lord',
-  statCaps: { cha: 38, def: 40, dex: 25, hp: 65, lck: 30, mag: 22, res: 22, spd: 22, str: 45 },
   tier: 'Unique',
 };
 
@@ -441,7 +399,6 @@ const EMPEROR: ClassData = {
   id: 'emperor',
   movement: 5,
   name: 'Emperor',
-  statCaps: { cha: 40, def: 48, dex: 28, hp: 75, lck: 32, mag: 22, res: 22, spd: 25, str: 55 },
   tier: 'Unique',
 };
 
@@ -451,7 +408,6 @@ const HIGH_LORD: ClassData = {
   id: 'high-lord',
   movement: 5,
   name: 'High Lord',
-  statCaps: { cha: 40, def: 26, dex: 32, hp: 60, lck: 35, mag: 22, res: 22, spd: 32, str: 45 },
   tier: 'Unique',
 };
 
@@ -461,7 +417,6 @@ const GREAT_LORD: ClassData = {
   id: 'great-lord',
   movement: 6,
   name: 'Great Lord',
-  statCaps: { cha: 42, def: 28, dex: 40, hp: 70, lck: 38, mag: 22, res: 22, spd: 42, str: 55 },
   tier: 'Unique',
 };
 
@@ -471,7 +426,6 @@ const WYVERN_MASTER: ClassData = {
   id: 'wyvern-master',
   movement: 7,
   name: 'Wyvern Master',
-  statCaps: { cha: 40, def: 33, dex: 35, hp: 60, lck: 35, mag: 18, res: 18, spd: 30, str: 42 },
   tier: 'Unique',
 };
 
@@ -481,7 +435,6 @@ const BARBAROSSA: ClassData = {
   id: 'barbarossa',
   movement: 8,
   name: 'Barbarossa',
-  statCaps: { cha: 42, def: 36, dex: 42, hp: 68, lck: 38, mag: 18, res: 18, spd: 33, str: 50 },
   tier: 'Unique',
 };
 
@@ -491,7 +444,6 @@ const ENLIGHTENED_ONE: ClassData = {
   id: 'enlightened-one',
   movement: 5,
   name: 'Enlightened One',
-  statCaps: { cha: 45, def: 30, dex: 38, hp: 70, lck: 40, mag: 35, res: 30, spd: 35, str: 42 },
   tier: 'Unique',
 };
 
