fd17a0dcadf8e05206c96ea84eb929ef7d683055
hokkyss <hokkyss2@gmail.com>
2026-06-27 21:35:39 +0700

    fix: unfriendly to mobile devices (#209)

--- apps/public-tanstack-start/src/lib/common/functions/render-markdown.function.tsx
+++ apps/public-tanstack-start/src/lib/common/functions/render-markdown.function.tsx
@@ -1,3 +1,4 @@
+import cn from '@portfolio/design-system/cn';
 import tw from '@portfolio/design-system/tw';
 import remarkCallout from '@portfolio/markdown/callout';
 import { createServerFn } from '@tanstack/react-start';
@@ -21,13 +22,16 @@ const renderMarkdownFunction = createServerFn({
     setResponseHeader('X-Stale-After', '604800');
 
     return renderServerComponent(
-      <article className={tw`prose prose-neutral dark:prose-invert black-eagles:prose-rose black-eagles:prose-invert blue-lions:prose-blue blue-lions:prose-invert golden-deer:prose-amber max-w-none`}>
+      <article className={tw`prose prose-neutral dark:prose-invert black-eagles:prose-rose black-eagles:prose-invert blue-lions:prose-blue blue-lions:prose-invert golden-deer:prose-amber`}>
         <ReactMarkdown
           components={{
             a: (props) => (
               <a {...props} rel="noopener noreferrer" target="_blank" />
             ),
             div: MarkdownCallout,
+            pre: ({ className, ...props }) => (
+              <pre className={cn(tw`grid`, className)} {...props} />
+            ),
           }}
           rehypePlugins={[rehypeHighlight]}
           remarkPlugins={[remarkGfm, remarkGemoji, remarkCallout]}
