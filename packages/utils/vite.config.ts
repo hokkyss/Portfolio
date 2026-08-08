70cecd9b2e111202caa960fd669bf5e3bcd07df2
hokkyss <hokkyss2@gmail.com>
2026-08-08 18:21:26 +0700

    refactor: update utils build (#225)

--- packages/utils/vite.config.ts
+++ packages/utils/vite.config.ts
@@ -1,12 +1,64 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
 
 export default defineConfig({
   build: {
     lib: {
-      entry: {
-        index: "./lib/index",
-      },
-      formats: ["es"],
+      entry: './lib/index',
+      formats: ['es'],
     },
+  },
+  builder: {
+    async buildApp(builder) {
+      await builder.build(builder.environments.ssr);
+      await builder.build(builder.environments.rsc);
+      await builder.build(builder.environments.client);
+    },
+  },
+  environments: {
+    client: {
+      build: {
+        emptyOutDir: true,
+        minify: 'oxc',
+        outDir: 'dist/client',
+        rolldownOptions: {
+          plugins: [
+            dts({
+              exclude: ['./*'],
+              outDir: './dist/types',
+            }),
+          ],
+        },
+        sourcemap: true,
+      },
+      consumer: 'client',
+    },
+    rsc: {
+      build: {
+        emptyOutDir: true,
+        minify: 'oxc',
+        outDir: 'dist/rsc',
+        sourcemap: true,
+      },
+      consumer: 'server',
+      resolve: {
+        noExternal: true,
+      },
+    },
+    ssr: {
+      build: {
+        emptyOutDir: true,
+        minify: 'oxc',
+        outDir: 'dist/ssr',
+        sourcemap: true,
+      },
+      consumer: 'server',
+      resolve: {
+        noExternal: true,
+      },
+    },
+  },
+  resolve: {
+    tsconfigPaths: true,
   },
 });
